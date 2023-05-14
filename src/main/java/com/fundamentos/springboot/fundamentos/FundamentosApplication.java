6e118888a700d279322a6211d5dfd771b92e6e29
angelbedo <angelbedo104@gmail.com>
2023-05-13 23:28:40 -0500

    "AVazando en el curso"

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,34 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
+import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+	private ComponentDependency componentDependency;
+	private MyBean myBean;
+
+	private MyBeanWithProperties myBeanWithProperties;
+
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithProperties myBeanWithProperties){
+		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithProperties = myBeanWithProperties;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+		myBean.print();
+		System.out.println(myBeanWithProperties.function());
+	}
 }
